ba1b7b1bfb30a5d09405e37debd5e500a98df15d
Jose Valenzuela <valenz74@gmail.com>
2022-05-19 18:17:33 -0500

    Aprendiendo manejo de Decimales en Java

---
+++ src/Decimales.java
@@ -0,0 +1,10 @@
+public class Decimales {
+    public static void main(String[] args) {
+        float a = 10.5f;
+        float b = 1.5f;
+        float c = a + b;
+
+        System.out.println(c);
+
+    }
+}
